0d00274cbcc3d119d4430201fd50b36b043dcb1f
shadmau <17837186+shadmau@users.noreply.github.com>
2026-02-26 06:37:27 +0100

    update site URL, add favicon and meta data

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,5 +1,13 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://modelarena.dev",
+  site: "https://modelarena.tv",
+  build: {
+    assets: "_assets",
+  },
+  vite: {
+    build: {
+      cssMinify: true,
+    },
+  },
 });
